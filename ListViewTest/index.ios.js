42d7a358cc33043ea254c0f9f4c227e80fbabed2
sabazusi <kouhei.live@gmail.com>
2017-05-05 22:01:30 +0900

    Add gifted-listview, and load automatically

--- ListViewTest/index.ios.js
+++ ListViewTest/index.ios.js
@@ -1,31 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// @flow
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
+import GiftedListView from 'react-native-gifted-listview';
+
+const fetchItem = (page) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(page === 1 ? initialData : continueData(page)), 1000);
+  });
+};
 
 export default class ListViewTest extends Component {
+  _renderRowView(data) {
+    return (
+      <View>
+        <Text style={{fontSize: 18, margin: 10}}>{data.title}</Text>
+      </View>
+    );
+  }
+
+  _onFetch(page = 1, callback, options) {
+    fetchItem(page)
+      .then((data) => {
+        callback(data, {
+          allLoaded: false
+        });
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={{height: 64, backgroundColor: '#CCC'}} />
+        <GiftedListView
+          ref={(ref) => this.listViewRef = ref}
+          rowView={this._renderRowView}
+          onFetch={this._onFetch}
+          refreshable={true}
+          enableEmptySections={true}
+          paginationFetchingView={() => null}
+          paginationWaitingView={() => null}
+          customStyles={{
+            paginationView: {
+              backgroundColor: '#eee',
+            },
+          }}
+          refreshableTintColor="blue"
+          onEndReached={() => this.listViewRef._onPaginate()}
+          onEndReachedThreshold={1}
+        />
       </View>
     );
   }
@@ -34,20 +63,48 @@ export default class ListViewTest extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#f6b585',
   },
 });
 
+const initialData = [
+  {title: 'title-1'},
+  {title: 'title-2'},
+  {title: 'title-3'},
+  {title: 'title-4'},
+  {title: 'title-5'},
+  {title: 'title-6'},
+  {title: 'title-7'},
+  {title: 'title-8'},
+  {title: 'title-9'},
+  {title: 'title-10'},
+  {title: 'title-11'},
+  {title: 'title-12'},
+  {title: 'title-13'},
+  {title: 'title-14'},
+  {title: 'title-15'},
+  {title: 'title-16'},
+  {title: 'title-17'},
+  {title: 'title-18'},
+  {title: 'title-19'},
+  {title: 'title-20'},
+  {title: 'title-21'},
+  {title: 'title-22'},
+  {title: 'title-23'},
+  {title: 'title-24'},
+  {title: 'title-25'},
+  {title: 'title-26'},
+  {title: 'title-27'},
+  {title: 'title-28'},
+  {title: 'title-29'},
+  {title: 'title-30'}
+];
+
+const continueData = (page) => [
+  {title: `title-${30 + (page - 1) * 5 - 4}`},
+  {title: `title-${30 + (page - 1) * 5 - 3}`},
+  {title: `title-${30 + (page - 1) * 5 - 2}`},
+  {title: `title-${30 + (page - 1) * 5 - 1}`},
+  {title: `title-${30 + (page - 1) * 5}`},
+];
 AppRegistry.registerComponent('ListViewTest', () => ListViewTest);
